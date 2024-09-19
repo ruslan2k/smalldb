106d84e1488e2ae2992e7c5a0d0e8c6b1a95f00e
Ruslan <ruslan@example.com>
2024-09-19 23:18:01 +0300

    WiP

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { SmallDb } from './smalldb'
--- src/smalldb.ts
+++ src/smalldb.ts
@@ -1,6 +1,25 @@
+const SMALLDB_KEY = 'SMALLDB_KEY'
+
 export class SmallDb<T> {
-  data: T
+  data: T | null
+
   constructor (data: T) {
     this.data = data
+  }
+
+  async read (): Promise<void> {
+    const dataStr = localStorage.getItem(SMALLDB_KEY)
+    if (dataStr === null) {
+      this.data = null
+      return
+    }
+    this.data = JSON.parse(dataStr)
+  }
+
+  async write (): Promise<void> {
+    if (this.data === null) {
+      return
+    }
+    localStorage.setItem(SMALLDB_KEY, JSON.stringify(this.data))
   }
 }
